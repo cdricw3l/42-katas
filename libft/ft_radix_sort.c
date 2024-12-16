18a132f8743d5e882aca78956002ace681abb989
cw3l <cw3l@MacBook-Pro-de-cw3l.local>
2024-12-16 12:04:41 +0100

    libft.a/

--- libft/ft_radix_sort.c
+++ libft/ft_radix_sort.c
@@ -6,7 +6,7 @@
 /*   By: cw3l <cw3l@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/13 18:48:45 by cw3l              #+#    #+#             */
-/*   Updated: 2024/12/15 19:21:02 by cw3l             ###   ########.fr       */
+/*   Updated: 2024/12/16 12:04:39 by cw3l             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -48,67 +48,47 @@ void	ft_arr_of_count_digt(int *arr, int *arr_count, int len, int exp)
 	{
 		while (i < len)
 		{
-			if (arr[i] / exp > 0)
-			{
-				digit = (arr[i] / exp) % 10;
-				arr_count[digit] += 1;
-			}
+			digit = (arr[i] / exp) % 10;
+			arr_count[digit]++;
 			i++;
 		}
 	}
-
-}
-
-void	ft_cumulatif_arr(int *count_digit, int *cumulatif_arr, int len)
-{
-	int	i;
-
-	i = 0;
+	i = 1;
 	while (i < len)
 	{
-		if (i == 0)
-			cumulatif_arr[i] = count_digit[i];
-		else
-			cumulatif_arr[i] = count_digit[i] + cumulatif_arr[i - 1];
+		arr_count[i] += arr_count[i - 1];
 		i++;
 	}
 }
 
 void	ft_count_sort(int *arr, int *tmp, int len, int exp)
 {
-	int	count_digit[10];
 	int	cumulatif_arr[10];
-	int	digit;
-	int	index;
 
-	ft_bzero(count_digit,10, sizeof(int));
 	ft_bzero(cumulatif_arr,10, sizeof(int));
-	ft_arr_of_count_digt(arr, count_digit, len, exp);
-	ft_cumulatif_arr(count_digit, cumulatif_arr, 10);
-	while (len >= 1)
+	ft_arr_of_count_digt(arr, cumulatif_arr, len, exp);
+	ft_print(cumulatif_arr,10);
+	while (len - 1 > 0)
 	{
-		digit = (tmp[len - 1] / exp) % 10;
-		index = cumulatif_arr[digit] - 1;
-		cumulatif_arr[digit]--;
-		arr[index] = tmp[len - 1];
-		len--;
+		arr[(cumulatif_arr[(tmp[len - 1] / exp) % 10]) - 1] = tmp[len -1];
+		cumulatif_arr[(arr[len - 1] / exp) % 10]--;
 	}
-
+	ft_print(tmp, 10);
 }
 
 void	ft_radix_sort(int *arr, int len)
 {
 	int	exp;
-	int	max_value;
+	//int	max_value;
 	int	*tmp;
 
-	exp = 1;
-	max_value = ft_get_max_value(arr, len);
+	exp = 10;
+	//max_value = ft_get_max_value(arr, len);
 	tmp = malloc(len * sizeof(int));
 	if (!tmp)
 		return ;
 	ft_copy_arr_int(arr, tmp, len);
-	while (max_value / exp > 0)
+	while (exp == 10)
 	{
 		ft_count_sort(arr, tmp, len, exp);
 		exp *= 10;
@@ -117,63 +97,16 @@ void	ft_radix_sort(int *arr, int len)
 
 int main(void)
 {
-	int arr[10] = {4, 7, 8, 6, 3, 10, 2, 9, 1, 5};
-	int	count_digit[10];
-	int	cumulatif_arr[10];
-	
-	int *tmp;
-	tmp = malloc(10 * sizeof(int));
-	if (!tmp)
-		return 1;
-	//ft_radix_sort(arr, 10);
-
-	ft_arr_of_count_digt(arr,count_digit,10,1);
-	ft_cumulatif_arr(count_digit, cumulatif_arr, 10);
-	ft_print(count_digit, 10);
-	ft_print(cumulatif_arr, 10);
-
-	int len = 10;
-	int digit;
-	int index;
-	ft_copy_arr_int(arr,tmp,10);
-
-	while (len >= 1)
-	{
-		
-		digit = (tmp[len - 1] / 1) % 10;
-		printf("voici le digit %d\n", digit);
-		index = cumulatif_arr[digit] - 1;
-		cumulatif_arr[digit]--;
-		arr[index] = tmp[len - 1];
-		len--;
-	}
-	ft_print(arr, 10);
-	printf("\n seconde passe \n");
-
-	ft_bzero(count_digit,10,4);
-	ft_bzero(cumulatif_arr,10,4);
-	ft_arr_of_count_digt(arr,count_digit,10,1);
-	ft_cumulatif_arr(count_digit, cumulatif_arr, 10);
-	ft_print(count_digit, 10);
-	ft_print(cumulatif_arr, 10);
-
-	len = 10;
-	ft_copy_arr_int(arr,tmp,10);
-	
-	while (len >= 1)
-	{
-		
-		digit = (tmp[len - 1] / 10) % 10;
-		printf("voici le digit %d\n", digit);
-		if(digit > 0)
-		{
-			index = cumulatif_arr[digit] - 1;
-			cumulatif_arr[digit]--;
-			arr[index] = tmp[len - 1];
-		}
-		len--;
-	}
+	int arr[] = {1000, 1, 200, 3, 44, 5, 650, 7, 8, 9};
+	ft_radix_sort(arr, 10);
 	ft_print(arr, 10);
 
+
+	// int	count_digit[10];
+	// int	cumulatif_arr[10];
+
+	// ft_arr_of_count_digt(arr, count_digit,10, 10);
+	// ft_print(count_digit, 10);
+	// ft_cumulatif_arr(count_digit,cumulatif_arr,10);
 
 }
