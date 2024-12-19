32f1e287a3e5972486f0e780a3ca98a91332c78e
Cedric Bouhadra <cbouhadr@c2r9s10.42lausanne.ch>
2024-12-19 17:09:53 +0100

    push_swap/Thu_Dec_19_05:09:53_PM_CET_2024

--- so_long/include/so_long.h
+++ so_long/include/so_long.h
@@ -6,7 +6,7 @@
 /*   By: cbouhadr <cbouhadr@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/17 14:40:51 by cbouhadr          #+#    #+#             */
-/*   Updated: 2024/12/18 13:49:42 by cbouhadr         ###   ########.fr       */
+/*   Updated: 2024/12/19 17:06:22 by cbouhadr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,15 +29,25 @@ typedef struct s_dimention
 
 } t_dimention;
 
+typedef struct s_mouse_position
+{
+	int x;
+	int y;
+	
+} t_mouse_position;
+
 
 typedef struct s_data
 {
-	void        *img;
-	char        *addr;
-	int         bit_per_pixel;
-	int         line_length;
-	int         endian;
-	t_dimention dimention;
+	void				*mlx;
+	void				*window;
+	void				*img;
+	char				*addr;
+	int					bit_per_pixel;
+	int					line_length;
+	int					endian;
+	t_dimention			dimention;
+	t_mouse_position	mouse_position;
 
 } t_data;
 
@@ -46,14 +56,14 @@ void    ft_print_map(char **map, int hauteur, int largeur);
 
 
 // check map
-int     ft_check_params(char **map, int  hauteur, int largeur);
-int     is_rectangle(int hauteur, int largeur);
-int     are_exit(char **map, int  hauteur, int largeur);
-int     are_item(char **map, int  hauteur, int largeur);
-int     are_start(char **map, int  hauteur, int largeur);
-int     is_close(char **map, int  hauteur, int largeur);
-int		is_empty_or_wrong(char **map, int hauteur, int largeur);
+int		ft_check_params(char **map, int check_arr[5], int hauteur, int largeur);;
+int		is_close_and_rectangle(char **map, int hauteur, int largeur);
 char    **ft_parse_params(char *file, int hauteur, int largeur);
 char    *ft_error_return(int error);
+int		ft_colors(int larg, int lo);
+
+int ft_manage_mouse(int keycode, t_data *data);
+int ft_manage_keyboard(int keycode, t_data *data);
+int ft_close_windows(int keycode, t_data **data);
 
 #endif
