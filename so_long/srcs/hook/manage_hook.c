e50c13b84dc0f446ef357a865b6cef535e4a6b07
cb <cdricw3l@gmail.com>
2024-12-22 08:25:07 +0100

    so_long

--- so_long/srcs/hook/manage_hook.c
+++ so_long/srcs/hook/manage_hook.c
@@ -6,7 +6,7 @@
 /*   By: cb <cb@student.42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/19 14:59:18 by cbouhadr          #+#    #+#             */
-/*   Updated: 2024/12/21 23:32:07 by cb               ###   ########.fr       */
+/*   Updated: 2024/12/22 08:23:53 by cb               ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,31 +14,44 @@
 
 
 
-void    update_charactere_position(t_data *data, int keycode)
+void    update_charactere_position(t_data *data, int keycode, int init_row, int init_col)
 {
-    t_dimention initial_position;
-    
-    initial_position = data->begin;
+        
     if(keycode == 97)
-        if (data->begin.row > 1 && data->map[data->begin.row - 1][data->begin.col] != '1')
+        if (data->map[data->begin.row - 1][data->begin.col] != '1')
             data->begin.row -= 1;
     if(keycode == 100)
-        if (data->begin.col < data->dimention.col - 2  && data->map[data->begin.row][data->begin.col + 1] != '1')
+        if (data->map[data->begin.row][data->begin.col + 1] != '1')
             data->begin.col += 1;
     if(keycode == 115)
-        if (data->begin.row < data->dimention.row - 2 && data->map[data->begin.row + 1][data->begin.col] != '1')
+        if (data->map[data->begin.row + 1][data->begin.col] != '1')
             data->begin.row += 1;
     if(keycode == 119)
-        if (data->begin.col > 1 && data->map[data->begin.row][data->begin.col - 1] != '1')
+        if (data->map[data->begin.row][data->begin.col - 1] != '1')
             data->begin.col -= 1;
-    if(initial_position.col != data->begin.col || initial_position.row != data->begin.row)
+    if(init_col != data->begin.col || init_row != data->begin.row)
     {
         data->count_mouvement++;
+        if(data->map[data->begin.row][data->begin.col] == 'X')
+        {
+            data->count_item--;
+            if (data->count_item == 0)
+            {
+                printf("end game \n");
+                exit(0);
+            }
+        }
+        data->map[data->begin.row][data->begin.col] = 'P';
+        data->map[init_row][init_col] = ' ';
         printf("mise a jour de la position du personnage huateur %d , largeur : %d\n",data->begin.row, data->begin.col);
         printf("Nombre de mouvement %d\n",data->count_mouvement);
+        ft_print_map(data->map,data->dimention.row,data->dimention.col,4);
     }
     else
+    {
         printf("Le personage ne bouge pas, sa position reste hauteur %d, largeur %d\n", data->begin.row,data->begin.col);
+        ft_print_map(data->map,data->dimention.row,data->dimention.col,4);
+    }
 }
 
 void ft_keycode_counter_handler(t_data *data,int keycode)
@@ -46,9 +59,9 @@ void ft_keycode_counter_handler(t_data *data,int keycode)
 
     printf("voici le keycode %d\n",keycode);
     if(keycode == 97 || keycode == 100)
-        update_charactere_position(data, keycode);
+        update_charactere_position(data, keycode,data->begin.row, data->begin.col);
     if(keycode == 115 || keycode == 119)
-        update_charactere_position(data, keycode); 
+        update_charactere_position(data, keycode, data->begin.row, data->begin.col); 
 }
 
 int ft_manage_keyboard(int keycode, t_data *data)
