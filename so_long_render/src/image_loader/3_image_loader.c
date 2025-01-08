3396277612d28453dbed36d7abf067bae4ca78dd
cb <cdricw3l@gmail.com>
2025-01-08 03:38:10 +0100

    push_swap/mer._08_janv._2025_03:38:10_CET

--- so_long_render/src/image_loader/3_image_loader.c
+++ so_long_render/src/image_loader/3_image_loader.c
@@ -6,7 +6,7 @@
 /*   By: cb <cb@student.42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/05 18:23:15 by cb                #+#    #+#             */
-/*   Updated: 2025/01/08 03:22:40 by cb               ###   ########.fr       */
+/*   Updated: 2025/01/08 03:38:07 by cb               ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,10 +21,11 @@ int	image_loader(char **path, t_data **data)
 	img = (*data)->img_set;
 	if(!(*data)->img_set)
 		return(1);
-	while (i < IMG_SET_SIZE - 1)
+	while (i < IMG_SET_SIZE)
 	{	
+		printf("voici path i %s et les dim %d et %d ", path[i], img[i]->width ,img[i]->height);
 		img[i]->img = mlx_xpm_file_to_image( (*data)->mlx, path[i],
-					&(img[i]->width), &(img[i]->height));
+				&(img[i]->width), &(img[i]->height));
 		printf("creation de la image %p\n",img[i]);
 		if (!img[i]->img)
 			return (1);
