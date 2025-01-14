0a3b515d9102e891fa098b30990ad51b4245091a
cb <cdricw3l@gmail.com>
2025-01-15 00:07:33 +0100

    monster_layer

--- so_long/src_bonus/image_layer/static_image_monster_layer.c
+++ so_long/src_bonus/image_layer/static_image_monster_layer.c
@@ -13,6 +13,29 @@
 #include "../../include/bonus/image_layer_bonus.h"
 #include <assert.h>
 
+int ft_get_offset(t_data *data, int k, int path)
+{
+    int check;
+
+    check = data->img_set_global[path]->width;
+    if(128 * k > check)
+        return(-1);
+    if(k == 0)
+        return(0);
+    else
+        return(128 * k);
+}
+int ft_get_path()
+{
+    static int f = 0;
+    
+    if(f % 2 == 0)
+        return(8);
+    else
+        return(9);
+    f++;
+}
+
 static void	my_mlx_pixel_put(t_img *dst, t_img *src, t_xy xyf)
 {
 	char	*d;
@@ -59,7 +82,6 @@ int ft_put_image_on_frame(t_data *data, t_img *im)
 		}
 		i++;
 	}
-    
 
     return(0);
 }
@@ -72,10 +94,10 @@ int ft_get_image_on_frame(t_data *data, t_img *new,int path, int offset)
 	char	*s;
 
     i = 0;
-    while (i < 128)
+    while (i < TILD_SIZE)
 	{
         j = 0;
-		while (j < 128)
+		while (j < TILD_SIZE)
 		{
 			s = data->img_set_global[path]->addr + (j * data->img_set_global[path]->line_length + (i + offset)
 					* (data->img_set_global[path]->bit_per_pixel / 8));
@@ -93,33 +115,28 @@ int ft_get_image_on_frame(t_data *data, t_img *new,int path, int offset)
 int	monster_draw(t_data *data)
 {
 	static int k = 0;
-    static int f = 0;
     int offset;
-    int path;
     t_img *new;
 
     if(k == 8)
-    {
         k = 0;
-        f++;
-    }
     new = calloc(1, sizeof(t_img));
-    new->img = mlx_new_image(data->mlx, 128, 128);
+    if(!new)
+        return(1);
+    new->img = mlx_new_image(data->mlx, TILD_SIZE, TILD_SIZE);
+    if(!new->img)
+        return(1);
     new->addr = mlx_get_data_addr(new->img,&new->bit_per_pixel, &new->line_length, &new->endian);
-    if(k == 0)
-        offset = 0;
-    else
-        offset = 128 * k;
-    if(f % 2 == 0)
-        path = 8;
-    else
-        path = 9;
-    ft_get_image_on_frame(data,new, path);
+    if(!new->img)
+        return(1);
+    offset = ft_get_offset(data,k++, ft_get_path());
+    if(offset == -1)
+        return(1);
+    ft_get_image_on_frame(data,new, ft_get_path(), offset);
     ft_put_image_on_frame(data, new);
     mlx_put_image_to_window(data->mlx,data->window,data->frame->img,0,0);
-    free(new->img);
+    mlx_destroy_image(data->mlx, new->img);
     free(new);
-    k++;
     usleep(100000);
     return(0);
 }
