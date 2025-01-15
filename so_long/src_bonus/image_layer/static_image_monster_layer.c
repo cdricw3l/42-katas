71645346293f6dd06089dc05721229d1294d6baa
cb <cdricw3l@gmail.com>
2025-01-15 11:59:40 +0100

    layer

--- so_long/src_bonus/image_layer/static_image_monster_layer.c
+++ so_long/src_bonus/image_layer/static_image_monster_layer.c
@@ -18,7 +18,7 @@ int ft_get_offset(t_data *data, int k, int path)
     int check;
 
     check = data->img_set_global[path]->width;
-    if(128 * k > check)
+    if(128 * k > check || path < 0)
         return(-1);
     if(k == 0)
         return(0);
@@ -55,7 +55,8 @@ static void	my_mlx_pixel_put(t_img *dst, t_img *src, t_xy xyf)
 			d = dst->addr + ((j + (xyf.row))
 					*dst->line_length + (i + (xyf.col))
 					*(dst->bit_per_pixel / 8));
-			*(unsigned int *)d = *(unsigned int *)s;
+            if(s && d)
+			    *(unsigned int *)d = *(unsigned int *)s;
 			j++;
 		}
 		i++;
@@ -100,12 +101,15 @@ int ft_get_image_on_frame(t_data *data, t_img *new,int path, int offset)
         j = 0;
 		while (j < TILD_SIZE)
 		{
-			s = data->img_set_global[path]->addr + (j * data->img_set_global[path]->line_length + (i + offset)
+			s = data->img_set_global[path]->addr
+                    + (j * data->img_set_global[path]->line_length
+                    + (i + offset)
 					* (data->img_set_global[path]->bit_per_pixel / 8));
 			d = new->addr + (j
 					* new->line_length + i
 					* (new->bit_per_pixel / 8));
-			*(unsigned int *)d = *(unsigned int *)s;
+            if(s && d)
+			    *(unsigned int *)d = *(unsigned int *)s;
 			j++;
 		}
 		i++;
@@ -113,33 +117,47 @@ int ft_get_image_on_frame(t_data *data, t_img *new,int path, int offset)
     return(0);
 }
 
+t_img *init_new_img(t_data *data,int x, int y)
+{
+    t_img *new;
+    new = calloc(1, sizeof(t_img));
+    if(!new)
+        return(NULL);
+    new->img = mlx_new_image(data->mlx, x, y);
+    if(!new->img)
+        return(NULL);
+    new->addr = mlx_get_data_addr(new->img,
+            &new->bit_per_pixel,
+            &new->line_length, &new->endian);
+    if(!new->addr)
+        return(NULL);
+    return(new);
+}
+
 int	monster_draw(t_data *data)
 {
 	static int k = 0;
     int offset;
+    int path;
     t_img *new;
 
     if(k == 8)
         k = 0;
-    new = calloc(1, sizeof(t_img));
-    if(!new)
+    new = init_new_img(data, TILD_SIZE, TILD_SIZE);
+    if(!new || !data || !data->frame || !data->img_set_global)
         return(1);
-    new->img = mlx_new_image(data->mlx, TILD_SIZE, TILD_SIZE);
-    if(!new->img)
+    path = ft_get_path(k++);
+    if(path < 8 || path > 9)
         return(1);
-    new->addr = mlx_get_data_addr(new->img,&new->bit_per_pixel, &new->line_length, &new->endian);
-    if(!new->img)
-        return(1);
-    offset = ft_get_offset(data, k, ft_get_path(k));
+    offset = ft_get_offset(data, k, path);
     if(offset == -1)
-        return(1);    
-    ft_get_image_on_frame(data,new, ft_get_path(k), offset);
+        return(1); 
+    ft_get_image_on_frame(data,new, path, offset);
     ft_put_image_on_frame(data, new);
     mlx_put_image_to_window(data->mlx,data->window,data->frame->img,0,0);
     mlx_destroy_image(data->mlx, new->img);
     free(new);
     usleep(100000);
-    k++;
     return(0);
 }
 
