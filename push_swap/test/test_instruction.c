9c71fcceca65de4a4a79e28668e8340539b84c6c
cb <cdricw3l@gmail.com>
2025-01-24 00:47:05 +0100

    push_swap/ven._24_janv._2025_00:47:05_CET

--- push_swap/test/test_instruction.c
+++ push_swap/test/test_instruction.c
@@ -24,8 +24,9 @@ int ft_test_push(void)
     stack_b = ft_new_stack(n, 0, 98);
     if(!stack_a || !stack_b)
         return(ft_clean_memory(&stack_a, &stack_b));
-    /* int g = ft_generate_number(stack_a->arr, stack_a->len);
-    assert(g == 0); */
+    int g = ft_generate_number(stack_a->arr, stack_a->len);
+    assert(g == 0);
+    ft_get_stack_data(stack_a);
     /* stack_a->arr[0] = 3;
     stack_a->arr[1] = 5;
     stack_a->arr[2] = 2;
@@ -36,10 +37,9 @@ int ft_test_push(void)
     stack_a->arr[2] = -3;
     stack_a->arr[3] = -54;
     stack_a->arr[4] = 5; */
-    ft_get_stack_data(stack_a);
     ft_five_value(stack_a, stack_b);
     ft_get_stack_data(stack_a);
-    ft_get_stack_data(stack_b);
+    printf(" ==> Nombre d'operation : %d \n", stack_a->mvm_counter + stack_b->mvm_counter);
     assert(ft_is_sort(stack_a->arr, 5, 4, ft_cmp_int) == 0);
     ft_clean_memory(&stack_a, &stack_b);
     return(0);
