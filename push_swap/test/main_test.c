f4a3b925bcf9dd3541ffa3626195f9f3abacd191
cw3l <cw3l@MacBook-Pro-de-cw3l.local>
2025-02-03 06:02:09 +0100

    push_swap/Lun__3_fév_2025_06:02:09_CET

--- push_swap/test/main_test.c
+++ push_swap/test/main_test.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   test_dispatch.c                                    :+:      :+:    :+:   */
+/*   main_test.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: cw3l <cw3l@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/18 15:14:58 by cw3l              #+#    #+#             */
-/*   Updated: 2024/11/18 16:00:50 by cw3l             ###   ########.fr       */
+/*   Updated: 2025/02/03 06:01:58 by cw3l             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,43 +23,62 @@ int main(void)
     int n;
 	t_pile *stack_a;
     t_pile *stack_b;
-	int start_point_a;
-	int start_point_b;
 
-    n = 500;
+
+    n = 10;
     stack_a = ft_new_stack(n, n, 97);
-    stack_b = ft_new_stack(n, 0, 98);
-    ft_generate_number(stack_a->arr, 500);
-    // stack_a->arr[0] = 9;
-    // stack_a->arr[1] = 19;
-    // stack_a->arr[2] = 223;
-    // stack_a->arr[3] = 8;
-    // stack_a->arr[4] = 409;
-    // stack_a->arr[5] = 22211;
-    // stack_a->arr[6] = 29;
-    // stack_a->arr[7] = 887;
-    // stack_a->arr[8] = 87;
-    // stack_a->arr[9] = 1;
+    stack_b = ft_new_stack(n, n, 98);
+    
+    stack_a->arr[0] = 9;
+    stack_a->arr[1] = 19;
+    stack_a->arr[2] = 223;
+    stack_a->arr[3] = 8;
+    stack_a->arr[4] = 409;
+    stack_a->arr[5] = 22211;
+    stack_a->arr[6] = 29;
+    stack_a->arr[7] = 887;
+    stack_a->arr[8] = 87;
+    stack_a->arr[9] = 1;
+
+    stack_b->arr[0] = 9;
+    stack_b->arr[1] = 19;
+    stack_b->arr[2] = 223;
+    stack_b->arr[3] = 8;
+    stack_b->arr[4] = 409;
+    stack_b->arr[5] = 22211;
+    stack_b->arr[6] = 29;
+    stack_b->arr[7] = 887;
+    stack_b->arr[8] = 87;
+    stack_b->arr[9] = 1;
 
 
-    start_point_a = stack_a->arr[0];
-	start_point_b = stack_b->arr[0];
+    
+    optimised_rotation(stack_a, 3);
+    assert(stack_a->mvm_counter == 3);
+    optimised_rotation(stack_a, 6);
+    assert(stack_a->mvm_counter == 7);
+    optimised_rotation(stack_b, 9);
+    assert(stack_b->mvm_counter == 1);
+    stack_a->mvm_counter = 0;
+    stack_b->mvm_counter = 0;
 
-    (void)start_point_a;
-    (void)start_point_b;
+	ft_print_arr(stack_a->arr,stack_a->len);
+	ft_print_arr(stack_b->arr,stack_b->len);
+    optimised_double_rotation(stack_a, stack_b, 3);
+    
+    
+    assert(stack_a->mvm_counter == 3 && stack_b->mvm_counter  == 0);
+	ft_print_arr(stack_a->arr,stack_a->len);
+	ft_print_arr(stack_b->arr,stack_b->len);
 
-
-    ft_fill_pill_b(stack_b, stack_a);
-    ft_fill_pill_b(stack_a, stack_b);
-    printf("voci le ombre de mouve %d et %d\n", stack_b->mvm_counter, stack_a->mvm_counter);
-    //ft_tree_values(stack_a);
-    ft_print_arr(stack_a->arr, stack_a->len);
-    ft_print_arr(stack_b->arr, stack_b->len);
-
-
-    //ft_fill_pill_a(stack_a, stack_b);
-	// ft_print_arr(stack_b->arr,stack_b->len);
-
+    printf("voici le nombre de mouvement %d\n", stack_a->arr[6]);
+  
+    optimised_double_rotation(stack_a, stack_b, 6);
+    printf("voici le nombre de mouvement %d\n", stack_a->mvm_counter);
+    
+	ft_print_arr(stack_a->arr,stack_a->len);
+	ft_print_arr(stack_b->arr,stack_b->len);
+    assert(stack_a->mvm_counter == 7 && stack_b->mvm_counter  == 0);
     ft_clean_memory(&stack_a, &stack_b);
 
 
