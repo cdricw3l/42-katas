651e7fc6c17ca0b339c4e13f5c4a86c19f490a9e
cw3l <cw3l@MacBook-Pro-de-cw3l.local>
2025-03-08 11:44:26 +0100

    push_swap/Sam__8_mar_2025_11:44:26_CET

--- philosopher/srcs/main.c
+++ philosopher/srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: cw3l <cw3l@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/18 10:15:41 by cbouhadr          #+#    #+#             */
-/*   Updated: 2025/03/08 11:38:17 by cw3l             ###   ########.fr       */
+/*   Updated: 2025/03/08 11:44:22 by cw3l             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,11 +23,12 @@ int	main(int argc, char **argv)
 	if (!arr_args)
 		return (1);
 
-	char *m = "1 0 0 1 0 1 0 0 0 1 1 1";
+	char *m_out = "1 0 0 0 1 0 0 0 2 0 1 0";
 	int ** tt;
 	int p = 4;
 	int t = 3;
-    tt = ft_str_to_matrice(m, p, t);
+	
+    tt = ft_str_to_matrice(m_out, p, t);
 	if(!tt)
 		return(1);
 	int i = 0;
