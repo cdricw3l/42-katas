8d054afae4a0673cd814a1f3f471ba023eb11071
cw3l <cw3l@MacBook-Pro-de-cw3l.local>
2025-03-10 00:26:00 +0100

    push_swap/Lun_10_mar_2025_00:26:00_CET

---
+++ pro_training/ml/init/base_python:Retour_de_fonction.md
@@ -0,0 +1,39 @@
+# Python permet plusieur type de retour:
+
+## Retourner une valeur unique
+
+    exemple: 
+
+    def addition(a, b):
+        return a + b
+
+## Retourner plusieurs valeurs  sous forme de tuple
+
+    exemple: 
+
+    def operations(a, b):
+        return a + b, a - b, a * b
+
+    resultat_add, resultat_sub, resultat_mul = operations(4, 2)
+    print(resultat_add, resultat_sub, resultat_mul)  # ➝ 6 2 8
+
+## Retourner une liste ou un dictionnaire
+
+    def liste_carres(n):\
+        return [i ** 2 for i in range(n)]
+
+    print(liste_carres(5))  # ➝ [0, 1, 4, 9, 16]
+
+    def infos_personne(nom, age):\
+        return {"nom": nom, "age": age}
+
+    print(infos_personne("Alice", 25))  # ➝ {'nom': 'Alice', 'age': 25}
+
+## Retourner None si aucune valeur n'est valide
+
+    🎯 Résumé
+    ✅ Retourner une seule valeur → return x
+    ✅ Retourner plusieurs valeurs → return x, y, z (tuple)
+    ✅ Retourner une liste ou un dictionnaire → [x, y, z] ou {"clé": valeur}
+    ❌ (à voir) Gérer les erreurs avec None ou try/except
+    ❌ (à voir)Utiliser yield pour générer des valeurs sans les stocker
--- pro_training/ml/init/project/main.py
+++ pro_training/ml/init/project/main.py
@@ -1,15 +1,52 @@
+import numpy as np
+import math as math
+
+
+
+def get_euclidian_d(p1, p2):
+    d = math.sqrt((p2[0] - p1[0])**2 + (p2[1] - p1[1])**2)
+    return d
+
+def get_matrice(city, mat):
+    for i in range(len(city)):
+        for j in range(len(city)):
+            mat[i][j] = round(get_euclidian_d(city[i], city[j]), 2)
+
+def ft_swap(arr, i, j):
+    tmp = arr[i]
+    arr[i] = arr[j]
+    arr[j] = tmp
+
+def get_parcour_value(arr, str):
+    value = 0.00
+    for i in range(len(arr) - 2):
+        #print("euclidian : ", get_euclidian_d(str[arr[i]], str[arr[i + 1]]))
+        value = value + get_euclidian_d(str[arr[i]], str[arr[i + 1]])
+    print("Valeur du chemin : ", arr, " : ", value)
+
+
+def permutation(arr,  idx, str):
+    if(idx == len(arr)):
+        get_parcour_value(arr, str)
+        return
+    for i in range(idx,len(arr)):
+        ft_swap(arr,idx,i)
+        permutation(arr, idx + 1, str)
+        ft_swap(arr,idx,i)
+
+
+
 def main():
-    str = "hello world"
-    len_str = len(str)
-    i = 0
-    while i < len_str:
-        print(str[i], end="")
-        i += 1
-    print()
-    return str, len_str
 
-
-if __name__ == "__main__":
-    str, len = main()
-    if str is not None:
-        print(str ,"de taille", len)
+    arr = [0,1,2,3]
+   # str = [[0.0, 0.0],[1.0, 0.0],[2.0, 0.0], [0.0, 1.0]]
+    str = [[0.0, 0.0],[1.0, 0.0],[2.0, 0.0], [0.0, 1.0]]
+    mat = np.empty((len(str) , len(str)), dtype=float)
+    get_matrice(str, mat)
+    #ft_arr_iter(str, 3, 3, 0, 0)
+    permutation(arr, 0,str)
+    # for i in mat:
+    #     for j in i:
+    #         print(j, end=" ")
+    #     print()
+main()
